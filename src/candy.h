070d39694224786d457553082839b56e92ea536a
goalworld <583032753@qq.com>
2014-04-16 12:49:36 +0800

    a

--- src/candy.h
+++ src/candy.h
@@ -4,17 +4,17 @@
 extern "C" {
 #endif
 
+#include "./base/candy_net_int.h"
+#include "./aio/candy_net.h"
 #include "./base/candy_error.h"
 #include "./base/candy_array.h"
 #include "./base/candy_queue.h"
 #include "./base/candy_buffer.h"
 #include "./base/candy_cycle_buffer.h"
+
 /*#include "./base/candy_thread.h"
 #include "./base/candy_thread_pool.h"
 #include "./base/candy_socket.h"*/
-#include "./base/candy_net_int.h"
-#include "./aio/candy_net.h"
-
 #ifdef __cplusplus
 }
 #endif
