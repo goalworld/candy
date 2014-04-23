f1dd8a0f9086861949bc99b24274e5ba65acde8b
goalworld <583032753@qq.com>
2014-04-23 10:59:24 +0800

    a

--- src/aio/candy_timerset.c
+++ src/aio/candy_timerset.c
@@ -31,7 +31,7 @@ int candy_timerset_execute(struct candy_timerset* timer){
 			continue;
 		}
 		clock_t df = now - ev->pre - ev->timeout;
-		CANDY_INFO(size,df);
+		CANDY_INFO("%d %d",size,df);
 		if(df >= 0 ){
 			if(!ev->brepeat){
 				candy_array_earse(&timer->arr,timer->cur,1,NULL);
