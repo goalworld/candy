cca4d0c2e8924c2500bb614e6da4e9f3032b0a40
goalworld <583032753@qq.com>
2014-04-23 15:30:40 +0800

    a

--- linux/install.sh
+++ linux/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cp ./build/libcandy.so /usr/lib
+if [ ! -x "/usr/lib64"]; then
+cp ./build/libcandy.so /usr/lib64
+fi
+
 mkdir -p /usr/include/candy
 cp -r ../include/candy/*  /usr/include/candy/
